6d9787c435c15051b1b3ca1d6b811ab6820c9a40
Alper Birinci <190701119@stu.istinye.edu.tr>
2024-03-19 22:02:24 +0300

    FetchPickAndPlace Fix

--- Alper/FetchPickAndPlace/main.py
+++ Alper/FetchPickAndPlace/main.py
@@ -12,7 +12,6 @@ env = gym.make(environment_name, max_episode_steps=100, render_mode='rgb_array')
 log_path = os.path.join('Training', 'Logs')
 env = gym.make(environment_name)
 env = DummyVecEnv([lambda: env])
-#env = sb3_contrib.common.wrappers.TimeFeatureWrapper(env, max_steps=100)
 model = TQC('MultiInputPolicy', env, learning_rate=0.001, batch_size=512, gamma=0.98, tau=0.005, train_freq=1,
             learning_starts=1000, policy_kwargs=dict(net_arch=[512, 512], n_critics=2), replay_buffer_class=HerReplayBuffer,
             replay_buffer_kwargs=dict(n_sampled_goal=4, goal_selection_strategy='future'), verbose=1, tensorboard_log=log_path)
