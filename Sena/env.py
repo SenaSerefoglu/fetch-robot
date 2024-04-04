4f699cb483a488d3cc977f4ad9052fc545ecc1a0
Sena Serefoglu <sena.serefoglu.9@hotmail.com>
2024-04-04 18:04:28 +0300

    basic define_model function finished

--- Sena/env.py
+++ Sena/env.py
@@ -12,22 +12,35 @@ class Environment:
         self.env = DummyVecEnv([lambda: self.env])
         self.log_path = os.path.join('Logs', environment_name)
         
-    """def define_model(self, modelType=TQC, batch_size=1024, gamma=0.95, tau=0.005, train_freq=1, target_entropy=0.01, learning_starts=1000,
-                     policy_kwargs=dict(net_arch=[512, 512, 256, 128], n_critics=2), replay_buffer_class=HerReplayBuffer, buffer_size=1000000, 
-                     replay_buffer_kwargs=dict(n_sampled_goal=4, goal_selection_strategy='future'), verbose=1):
+    def define_model(self, model_type):
+        if self.environment_name == "FetchReach-v2" or self.environment_name == "FetchReachDense-v2":
+            if model_type == "PPO":
+                self.model = PPO('MultiInputPolicy', self.env, verbose=1, tensorboard_log=self.log_path)
+
+        elif self.environment_name == "FetchPush-v2" or self.environment_name == "FetchPushDense-v2":
+            if model_type == "TQC":
+                self.model = TQC('MultiInputPolicy',self.env, batch_size=1024, gamma=0.95, tau=0.005, train_freq=1, target_entropy=0.01,
+                                learning_starts=1000, policy_kwargs=dict(net_arch=[512, 512, 256, 128], n_critics=2), replay_buffer_class=HerReplayBuffer, buffer_size=1000000,
+                                replay_buffer_kwargs=dict(n_sampled_goal=4, goal_selection_strategy='future'), verbose=1, tensorboard_log=self.log_path)
+
+        elif self.environment_name == "FetchSlide-v2" or self.environment_name == "FetchSlideDense-v2":
+            if model_type == "TQC":
+                self.model = TQC('MultiInputPolicy',self.env, learning_rate=0.001, buffer_size=1000000, learning_starts=1000, batch_size=2048,
+                                tau=0.05, gamma=0.95, ent_coef=0.01, policy_kwargs=dict(n_critics=2, net_arch=[512, 512, 512]),
+                                replay_buffer_kwargs=dict(n_sampled_goal=4, goal_selection_strategy='future'), target_entropy='auto',
+                                replay_buffer_class=HerReplayBuffer, verbose=1, tensorboard_log=self.log_path)
         
-        self.model = modelType('MultiInputPolicy', self.env, batch_size=batch_size, gamma=gamma, tau=tau, train_freq=train_freq, target_entropy=target_entropy,
-                    learning_starts=learning_starts, policy_kwargs=policy_kwargs, replay_buffer_class=replay_buffer_class, buffer_size=buffer_size,
-                    replay_buffer_kwargs=replay_buffer_kwargs, verbose=verbose, tensorboard_log=self.log_path)"""
-    
+        elif self.environment_name == "FetchPickAndPlace-v2" or self.environment_name == "FetchPickAndPlaceDense-v2":
+            if model_type == "TQC":
+                self.model = TQC('MultiInputPolicy', self.env, learning_rate=0.001, batch_size=512, gamma=0.98, tau=0.005, train_freq=1,
+                                learning_starts=1000, policy_kwargs=dict(net_arch=[512, 512], n_critics=2), replay_buffer_class=HerReplayBuffer,
+                                replay_buffer_kwargs=dict(n_sampled_goal=4, goal_selection_strategy='future'), verbose=1, tensorboard_log=self.log_path)
 
     def train_model(self, timesteps=1000000):
         self.model.learn(total_timesteps=timesteps)
-        
-    def save_params(self): #Bitir
-        params = self.model.get_parameters()
     
-    def save_model(self, modelPath): # yolu degistir
+    def save_model(self): 
+        modelPath = os.path.join(self.log_path, 'Saved Models')
         self.model.save(modelPath)
         
     def evaluate_model(self, modelPath):
