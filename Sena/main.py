e561a3f7625b76e15ea61224a428f8c85409f6ef
Sena Serefoglu <sena.serefoglu.9@hotmail.com>
2024-04-04 18:04:54 +0300

    file name change

--- Sena/main.py
+++ Sena/main.py
@@ -1,15 +1,11 @@
 from tuner import Tuner
 from env import Environment
-import gymnasium as gym
-import os
 
 if __name__ == "__main__":
     environment_name = 'FetchReach-v2'
-    environment = Environment(environment_name, max_episode_steps=100, render_mode='human')
-    env = environment.env
-    log_path = environment.log_path
-    tuner = Tuner(env, log_path)
+    environment = Environment(environment_name, max_episode_steps=100, render_mode='rgb_array')
+    tuner = Tuner(environment)
 
-    #tuner.tunePPO(max_timesteps=500000, max_iterations=10, max_time=3600, save_model=True)
+    tuner.tune(model_type="PPO", max_timesteps=500000, max_iterations=10, max_time=3600, save_model=True)
 
-    environment.train_model(timesteps=1000)
+    #environment.train_model(timesteps=1000)
